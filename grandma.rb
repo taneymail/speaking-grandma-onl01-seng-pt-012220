b3181d949ec73e6ab430d296f31e6420ed834b54
TShipley <taneymail@gmail.com>
2020-01-26 23:29:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(speech)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+  if speech = 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
